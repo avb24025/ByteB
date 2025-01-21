c30749dfe13ad61443460848c9ab84930deb15f0
avb24025 <avb24022005@gmail.com>
2025-01-22 02:03:02 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://byte-blog-ebon.vercel.app/", // Backend server URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
